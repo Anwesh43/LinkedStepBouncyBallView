465e823857c719c81bd5de985ac1008e219521f9
Anwesh43 <anweshthecool0@gmail.com>
2020-05-21 06:50:58 +0530

    adding constants and libraries

--- stepbouncyballview/src/main/java/com/anwesh/uiprojects/stepbouncyballview/StepBouncyBallView.kt
+++ stepbouncyballview/src/main/java/com/anwesh/uiprojects/stepbouncyballview/StepBouncyBallView.kt
@@ -3,3 +3,17 @@ package com.anwesh.uiprojects.stepbouncyballview
 /**
  * Created by anweshmishra on 21/05/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+
+val colors : Array<String> = arrayOf("#3F51B5", "#9C27B0", "#4CAF50", "#FF9800", "#F44336")
+val balls : Int = 5
+val scGap : Float = 0.02f / balls
+val backColor : Int = Color.parseColor("#BDBDBD")
+val delay : Long = 20
