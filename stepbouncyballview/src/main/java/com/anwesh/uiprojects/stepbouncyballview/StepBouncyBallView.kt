1e140c5b9e3d9d2a17f2c456b5ae2c1732935115
Anwesh43 <anweshthecool0@gmail.com>
2020-05-21 07:00:43 +0530

    adding StepBouncyBallView class

--- stepbouncyballview/src/main/java/com/anwesh/uiprojects/stepbouncyballview/StepBouncyBallView.kt
+++ stepbouncyballview/src/main/java/com/anwesh/uiprojects/stepbouncyballview/StepBouncyBallView.kt
@@ -40,3 +40,19 @@ fun Canvas.drawSBBNode(i : Int, scale : Float, paint : Paint) {
     paint.color = Color.parseColor(colors[i])
     drawStepBouncyBalls(scale, w, h, paint)
 }
+
+class StepBouncyBallView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
