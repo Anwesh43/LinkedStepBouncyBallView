70db23d739bbed55bde072340a98512238c1554c
Anwesh43 <anweshthecool0@gmail.com>
2020-05-21 06:56:57 +0530

    adding Canvas  extension method

--- stepbouncyballview/src/main/java/com/anwesh/uiprojects/stepbouncyballview/StepBouncyBallView.kt
+++ stepbouncyballview/src/main/java/com/anwesh/uiprojects/stepbouncyballview/StepBouncyBallView.kt
@@ -22,3 +22,21 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawStepBouncyBalls(scale : Float, w : Float, h : Float, paint : Paint) {
+    val r : Float = w / (2 * colors.size)
+    for (j in 0..(balls - 1)) {
+        val sc : Float = scale.divideScale(j, balls).sinify()
+        save()
+        translate(2 * r * j + r, h / 2)
+        drawCircle(0f, 0f, r * sc, paint)
+        restore()
+    }
+}
+
+fun Canvas.drawSBBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = Color.parseColor(colors[i])
+    drawStepBouncyBalls(scale, w, h, paint)
+}
